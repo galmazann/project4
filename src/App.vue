6e3b9f69bdc95ee91c2a45bdfcb1fb2451364067
Gonzalo Almazan <galmazan@stu.southtexascollege.edu>
2024-09-22 21:32:59 -0500

    setup for recipe list

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>Recipe Expense Tracker</h1>
+    <ExpenseTracker @add-recipe="addRecipe"/>
+    <RecipeList :recipes="recipes" @select-recipe="selectRecipe"/>
+    <TotalCost :recipe="selectedRecipe"/>
+  </div>
+</template>
+
+<script>
+import RecipeList from './components/RecipeList.vue';
+import ExpenseTracker from './components/ExpenseTracker.vue';
+import TotalCost from './components/TotalCost.vue';
+
+export default {
+  components: { RecipeList, ExpenseTracker, TotalCost },
+  data() {
+    return {
+      recipes: [],
+      selectedRecipe: null,
+    };
+  },
+  methods: {
+    addRecipe(recipe) {
+      this.recipes.push(recipe);
+    },
+    selectRecipe(recipe) {
+      this.selectedRecipe = recipe;
+    },
+  },
+};
+</script>
---
+++ src/components/RecipeList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h2>Recipes</h2>
+      <ul>
+        <li v-for="recipe in recipes" :key="recipe.name" @click="selectRecipe(recipe)">
+          {{ recipe.name }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['recipes'],
+    methods: {
+      selectRecipe(recipe) {
+        this.$emit('select-recipe', recipe);
+      },
+    },
+  };
+  </script>
+  
