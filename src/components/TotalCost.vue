b0c96c538d7eb429905f9c24f31891608882be51
Gonzalo Almazan <galmazan@stu.southtexascollege.edu>
2024-09-22 21:34:08 -0500

    setup for total cost

---
+++ src/components/TotalCost.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-if="recipe">
+      <h2>Total Cost for {{ recipe.name }}</h2>
+      <p>Cost: {{ totalCost }} USD</p>
+      <p>Calories: {{ totalCalories }} kcal</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['recipe'],
+    computed: {
+      totalCost() {
+        return this.recipe ? this.recipe.cost : 0;
+      },
+      totalCalories() {
+        return this.recipe ? this.recipe.calories : 0;
+      },
+    },
+  };
+  </script>
+  
