ca7ad73135828e7c5f4f66cabba774cb36b63a84
Gonzalo Almazan <galmazan@stu.southtexascollege.edu>
2024-09-22 21:33:36 -0500

    setup for expense tracker

---
+++ src/components/ExpenseTracker.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <h2>Add a New Recipe</h2>
+      <input v-model="newRecipe.name" placeholder="Recipe Name"/>
+      <input v-model.number="newRecipe.cost" placeholder="Total Cost"/>
+      <input v-model.number="newRecipe.calories" placeholder="Total Calories"/>
+      <button @click="submitRecipe">Add Recipe</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        newRecipe: { name: '', cost: 0, calories: 0 },
+      };
+    },
+    methods: {
+      submitRecipe() {
+        if (this.newRecipe.name) {
+          this.$emit('add-recipe', { ...this.newRecipe });
+          this.newRecipe = { name: '', cost: 0, calories: 0 };
+        }
+      },
+    },
+  };
+  </script>
+  
